86268429f9c5d1578f304b49d9c7b59713be90dd
abdizamedmo <abdizamedmo@gmail.com>
2024-01-20 23:58:59 +0300

    Added Utils in password hashing and compare (hash, plain)

--- models/users.go
+++ models/users.go
@@ -1,8 +1,26 @@
 package models
 
+import (
+	"github.com/aaqyaar/telemedicine/utils"
+	"gorm.io/gorm"
+)
+
 type User struct {
-	id       string `gorm:"primaryKey"`
-	name     string
-	email    string `gorm:"uniqueIndex"`
-	password string
+	gorm.Model
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `gorm:"unique" json:"email"`
+	Password  string `json:"password"`
+	Role      string `json:"role"`
+}
+
+func (u *User) BeforeSave(
+	db *gorm.DB,
+) error {
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = hashedPassword
+	return nil
 }
--- utils/helpers.go
+++ utils/helpers.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"errors"
 	"os"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func GetEnv(key string) string {
@@ -11,4 +13,17 @@ func GetEnv(key string) string {
 		panic(errors.New(key + " Environment variable not set"))
 	}
 	return value
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func ComparePassword(hashedPassword string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
 }
