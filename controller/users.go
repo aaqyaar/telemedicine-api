37f09a63b04efec285067427e776a84db5e0baae
abdizamedmo <abdizamedmo@gmail.com>
2024-01-29 14:42:10 +0300

    Added database fetching using gorm and postgres

--- controller/users.go
+++ controller/users.go
@@ -1,9 +1,37 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/aaqyaar/telemedicine/models"
+	"github.com/aaqyaar/telemedicine/providers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func CreateUser(c *fiber.Ctx) error {
+
+	var user models.User
+
+	fmt.Println(user.FirstName)
+
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": err})
+	}
+
+	providers.DB.Create(&user)
+
+	return c.JSON(fiber.Map{
+		"message": "User Created success",
+	})
+}
 
 func GetUsers(c *fiber.Ctx) error {
-	return c.SendString("All Users")
+
+	var users []models.User
+
+	providers.DB.Find(&users)
+
+	return c.JSON(fiber.Map{"data": users})
 }
 
 func GetUser(c *fiber.Ctx) error {
